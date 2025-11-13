f1103f47eddd951d0c66223abca43c7ebc6d2e6a
Mohammed Roqa <mroqa@outlook.com>
2025-11-13 21:47:11 +0200

    Add view to the module

--- extra-addons/custom_product_pricing/__manifest__.py
+++ extra-addons/custom_product_pricing/__manifest__.py
@@ -1,7 +1,17 @@
 {
-    'name': 'Custom Product Pricing',
-    'version': '1.0',
-    'depends': ['product'],
+    'name': 'Auto Product Pricing',
+    'version': '2.0',
+    'author': "Mohammed Roqa",
+    'category': 'Sales',
+    'summary': 'Automatic price calculation based on profit margin',
+    'description': """
+        This module adds a 'Profit Margin (%)' field to the product template.
+        It then automatically calculates the sales price based on the product's cost
+        and the specified profit margin.
+    """,
+    'depends': ['base', 'product'],
     'data': [],
     'installable': True,
+    'application': True,
+    'auto_install': False,
 }
